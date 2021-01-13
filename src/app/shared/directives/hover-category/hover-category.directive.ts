5c597fee20aeef624c29798f6c4263f526911cc4
DaniloLima122 <limadanilo06@gmail.com>
2021-01-13 00:04:15 -0300

    Added toggle at footer

--- src/app/shared/directives/hover-category/hover-category.directive.ts
+++ src/app/shared/directives/hover-category/hover-category.directive.ts
@@ -5,21 +5,7 @@ import { Directive, Renderer2, ElementRef } from '@angular/core';
 })
 export class HoverCategoryDirective{
 
-  // @Input() categoryElement!: ElementRef<any>;
-
   constructor(private render: Renderer2, private elementRef : ElementRef) { }
-
-
-  // ngAfterViewInit(){
-  //   this.categoryElement = this.elementRef;
-  // }
-
-  // @HostListener('mouseover')
-  // HoverOn() {
-
-  //   console.log(this.categoryElement)
-  // }
-
 
 
 
